51a3ba23ffebc0e7102f8281ced81c80f5628fe4
ThThelitz <thilo.thelitz@gmail.com>
2022-03-21 09:13:57 +0000

    Correct css paths, add gitignore

--- application.py
+++ application.py
@@ -20,7 +20,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index2.html")
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
