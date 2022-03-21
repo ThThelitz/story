b740b2bc609acb317c0bcfc15e9eb9dc86cd8f60
ThThelitz <thilo.thelitz@gmail.com>
2022-03-21 09:38:08 +0000

    Clean up initial code a bit

--- application.py
+++ application.py
@@ -5,18 +5,8 @@ Server for a digital narrative
 """
 from flask import Flask, render_template
 
-# # Library for enforcing HTTPS
-# # https://github.com/GoogleCloudPlatform/flask-talisman
-# from flask_talisman import Talisman
-
 app = Flask(__name__)
-
-# Ensure templates are auto-reloaded, i.e. reload templates when they are
-# changed. Not sure what this means exactly.
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-# # Setup possibly needed for stylesheets to work
-# app.static_folder = "static"
 
 @app.route("/")
 def index():
