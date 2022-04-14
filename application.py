1e8f101ab521ccc275f8ef692e362267a9b45b1b
ThThelitz <thilo.thelitz@gmail.com>
2022-04-14 09:14:11 +0100

    Clean up a line

--- application.py
+++ application.py
@@ -6,7 +6,6 @@ Server for a digital narrative
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def index():
@@ -17,4 +16,5 @@ def lexia_1():
     return render_template("1.html")
 
 if __name__ == "__main__":
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
     app.run(debug=True, use_reloader=False)
