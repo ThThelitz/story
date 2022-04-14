afaffc344eb45161a0634f5b0aff37b3ce392c5b
ThThelitz <thilo.thelitz@gmail.com>
2022-04-14 12:01:46 +0100

    Save password on button press

--- static/script.js
+++ static/script.js
@@ -1,6 +1,10 @@
-// localStorage.setItem("points", "0")
-// It works wtf
+let pwForm = document.getElementById("pw-form")
 
-function increasePoints() {
-    localStorage.setItem("points", "1")
-}
+pwForm.addEventListener("submit", function(ev) {
+    // Stores password in local storage on submit
+    // Saves emojis correctly, not 100% sure about encoding used
+    ev.preventDefault // does not work, but maybe I don't need it
+    let pw = document.getElementById("pw").value
+    localStorage.setItem("pw", pw)
+})
+
