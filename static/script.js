d7dfaa7817bf6b2fc5e27ea196a3973d10de323b
ThThelitz <thilo.thelitz@gmail.com>
2022-04-25 10:20:09 +0100

    Add brackets you dummy

--- static/script.js
+++ static/script.js
@@ -3,7 +3,7 @@ let pwForm = document.getElementById("pw-form")
 pwForm.addEventListener("submit", function(ev) {
     // Stores password in local storage on submit
     // Saves emojis correctly, not 100% sure about encoding used
-    ev.preventDefault // does not work, but maybe I don't need it
+    ev.preventDefault() // does not work, but maybe I don't need it
     let pw = document.getElementById("pw").value
     localStorage.setItem("pw", pw)
 })
