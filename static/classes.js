9c0a4a6c4d68d5372bf0a3a03bfa39ba8ca5cd06
ThThelitz <thilo.thelitz@gmail.com>
2022-05-19 17:18:30 +0100

    Factor out js

---
+++ static/classes.js
@@ -0,0 +1,15 @@
+class Module {
+    constructor(code, title, credits, full=true) {
+        this.code = code
+        this.title = title
+        this.credits = credits
+        this.full = full
+    }
+}
+
+class Room {
+    constructor(title, link) {
+        this.title = title
+        this.link = link
+    }
+}
---
+++ static/helpers.js
@@ -0,0 +1,13 @@
+// Choose n random elements from array (shuffled)
+// Actually, remove random elements until target length is reached
+function choose(array, n) {
+    rArray = [...array]
+    while (rArray.length > n) {
+        randomIndex = Math.floor(Math.random() * rArray.length)
+        rArray.splice(randomIndex, 1)
+    }
+
+    // Shuffle algorithm, see https://javascript.info/task/shuffle
+    rArray.sort(() => Math.random() - 0.5)
+    return rArray
+}
